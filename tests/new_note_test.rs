0d2a8a5de6f85bf8afccda3e80dcf43fb994fc92
Idan Attias <idana@wix.com>
2022-01-05 18:39:55 +0200

    test interactive mode for the new command

--- tests/new_note_test.rs
+++ tests/new_note_test.rs
@@ -53,28 +53,31 @@ fn test_new_note_custom_dir() {
     );
 }
 
-// TODO: test using https://crates.io/crates/rexpect
 #[test]
-#[ignore]
 fn test_new_note_interactive() {
     let setup = Setup::new();
     let expected_note_file_path = setup.dir.path().join("notes").join("chores");
 
     let mut cmd = Command::cargo_bin("gnotes").unwrap();
 
+    let mut stdin = String::new();
+
+    stdin.push('i'); // enter vim edit mode
+    stdin.push_str("do this and that");
+    stdin.push(27 as char); // ESC
+    stdin.push_str(":wq\n");
+
     cmd.args(vec!["new", "chores"])
+        .env("EDITOR", "vim")
         .env("GNOTES_NOTES_DIR", setup.dir.as_ref())
-        .write_stdin("i")
-        .write_stdin("do this and that")
-        .write_stdin(vec![27])
-        .write_stdin(":wq")
-        .write_stdin(vec![10])
+        .write_stdin(stdin)
         .assert()
         .success();
+
 
      assert!(expected_note_file_path.exists());
      assert_eq!(
         fs::read_to_string(expected_note_file_path).unwrap(),
-        String::from("do this and that")
+        String::from("do this and that\n")
     );
 }
